93cfe9740974760d1cae6926dd4a43ee0ab07c3a
onild <onildo.ninho@hotmail.com>
2019-06-26 19:57:22 -0300

    Primeira versão "funcional" do app, todos os botões antes da activity Principal estão funcionando"

--- app/src/main/java/br/uema/healthboss/MainActivity.java
+++ app/src/main/java/br/uema/healthboss/MainActivity.java
@@ -1,7 +1,10 @@
 package br.uema.healthboss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bt_pross = findViewById(R.id.bt_pross);
+        bt_pross.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
